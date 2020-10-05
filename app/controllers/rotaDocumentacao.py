8f96cc9e8407655f3b5d3f8152f682d0ab2c92c6
Benedicto Eduardo <dasilva.bedu@gmail.com>
2020-10-05 15:31:10 -0300

    voltando versão G.Doc

--- app/controllers/rotaDocumentacao.py
+++ app/controllers/rotaDocumentacao.py
@@ -15,3 +15,19 @@ def api_documentacao_agrupado(grupo):
 def api_documentacao_lista():
     resultado, retorno, header = gestaoDocumentacao.documentacaoLista()
     return jsonify(resultado), retorno, header
+
+
+@app.route("/seguranca_barragem/documento/<int:id>", methods=["GET","DELETE","PATCH"])
+def list_objects(id):
+    resultado, retorno, header = gestaoDocumentacao.documentacaoOriginalLeExcluiAltera(id)
+    return jsonify(resultado), retorno, header
+
+@app.route("/seguranca_barragem/documento", methods=["POST"])
+def put_objects():
+    resultado, retorno, header = gestaoDocumentacao.documentacaoOriginalInclui()
+    return jsonify(resultado), retorno, header
+
+@app.route("/seguranca_barragem/documento/chave", methods=["GET"])
+def chave():
+    resultado, retorno, header = gestaoDocumentacao.documentacaoOriginalChave()
+    return jsonify(resultado), retorno, header
